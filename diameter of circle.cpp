a4cc5f4e897e1a504f98b22d6088f228c0ddf6b9
Prachi9580 <p6836872@gmail.com>
2026-06-10 21:58:19 +0530

    Add files via upload

---
+++ diameter of circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r, d;
+
+    cout << "Enter radius: ";
+    cin >> r;
+
+    d = 2 * r;
+
+    cout << "Diameter = " << d;
+    return 0;
+}
