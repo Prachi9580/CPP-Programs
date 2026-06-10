6941b7ebdacbf51662f2366eb44fa3a5353a0147
Prachi9580 <p6836872@gmail.com>
2026-06-10 22:05:33 +0530

    Add files via upload

---
+++ perimeter of circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float r, perimeter;
+
+    cout << "Enter radius: ";
+    cin >> r;
+
+    perimeter = 2 * 3.14 * r;
+
+    cout << "Perimeter of Circle = " << perimeter;
+    return 0;
+}
